eb16539cab29495c77854f8bee9e33e59675ef1a
Staler2019 <abcd121234@yahoo.com.tw>
2021-01-02 04:44:24 +0800

    2021.1.2 4:44

---
+++ reference/KaratsubaMultiplication.h
@@ -0,0 +1,16 @@
+/*
+ * KaratsubaMultiplication.h
+ *
+ *  Created on: Feb 4, 2015
+ *      Author: Evin Ugur (http://evinugur.com)
+ */
+
+#ifndef KARATSUBAMULTIPLICATION_H_
+#define KARATSUBAMULTIPLICATION_H_
+
+int getLength(long long value); // returns the number of digits a number has
+long long multiply(long long x, long long y); // Karatsuba multiplication of two numbers
+
+
+
+#endif /* KARATSUBAMULTIPLICATION_H_ */
